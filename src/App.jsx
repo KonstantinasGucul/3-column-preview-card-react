09c0d98797b4996df4d813638481cb02b44799ed
Konstantinas Gucul <guculiux@gmail.com>
2025-03-05 11:38:44 +0000

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main>
+      <div className="container">
+        <h1>React Version of 3-Column Card Component</h1>
+      </div>
+    </main>
+  );
+}
